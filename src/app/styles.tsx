3b419ff1f9ff9416cd6647bf28229d7e73d6a2ec
David Grammatico <david.lugdunum@gmail.com>
2024-11-28 18:17:21 +0100

    lesson 1 : les styles

--- src/app/styles.tsx
+++ src/app/styles.tsx
@@ -1,6 +1,6 @@
 import { capitalizeFirstLetter } from '@/lib/formatString';
 import { Stack, usePathname } from 'expo-router';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, View, Text, ViewStyle } from 'react-native';
 
 export default function StylesScreen() {
 	const nameOfScreen = usePathname();
@@ -8,25 +8,59 @@ export default function StylesScreen() {
 	return (
 		<View style={styles.container}>
 			<Stack.Screen options={{ title: capitalizeFirstLetter(nameOfScreen) }} />
-			<View />
-			<View />
-			<View />
-			<View />
+			<View style={monStyle} />
+			<View style={mesStyles.view1} />
+			<View style={mesStyles.view2} />
+			<View style={{ backgroundColor: 'purple', height: 80, width: 50 }} />
+			<View style={styles.view3} />
+			<View
+				style={[styles.view3, { borderRadius: 80 }, { backgroundColor: 'red' }]}
+			/>
+			<Text style={{ textAlign: 'center' }}>Style de texte</Text>
 		</View>
 	);
 }
 
+// 0. Props Style pour chaque composant
+
 // 1. objet style (avec et sans satisfies)
+const monStyle = {
+	backgroundColor: 'red',
+	height: 100,
+	width: 100,
+	borderRadius: 50,
+} satisfies ViewStyle;
 
 // 2. objet avec plusieurs objets styles
+const mesStyles = {
+	view1: {
+		backgroundColor: 'green',
+		height: 100,
+		width: 100,
+		borderRadius: 50,
+	},
+	view2: {
+		backgroundColor: 'blue',
+		height: 100,
+		width: 100,
+		borderRadius: 50,
+	},
+};
 
 // 3. Style inline
 
 // 4. StyleSheet.create
 
+// .5 Tableau de styles (style condidionnel)
+
 const styles = StyleSheet.create({
 	container: {
 		flex: 1,
 		backgroundColor: '#ffffff',
+	},
+	view3: {
+		backgroundColor: 'orange',
+		height: 80,
+		width: 50,
 	},
 });
