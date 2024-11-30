e6910d8c96af0a7d843b932b963f8fbdc2efc8c6
David Grammatico <david.lugdunum@gmail.com>
2024-11-30 21:53:04 +0100

    add content for view page

--- src/app/view.tsx
+++ src/app/view.tsx
@@ -7,14 +7,50 @@ export default function ViewScreen() {
 
 	return (
 		<View style={styles.container}>
-			<Stack.Screen options={{ title: capitalizeFirstLetter(nameOfScreen) }} />
+			<Stack.Screen
+				options={{
+					title: capitalizeFirstLetter(nameOfScreen),
+				}}
+			/>
+			<View style={[styles.smallBox, { backgroundColor: 'violet' }]} />
+			<View style={[styles.bigBox, { backgroundColor: 'blue' }]} />
+			<View style={[styles.smallBox, { backgroundColor: 'green' }]} />
+			<View style={[styles.smallBox, { backgroundColor: 'purple' }]} />
+			<View style={[styles.bigBox, { backgroundColor: 'orange', flex: 0 }]} />
 		</View>
 	);
 }
 
+// 1. flex: 1 - 2, etc
+// 2. flexDirection
+// 3. justifyContent
+// 4. alignItems
+// 5. gap
+// 6. margin
+// 7. padding
+
 const styles = StyleSheet.create({
 	container: {
 		flex: 1,
-		backgroundColor: '#ffffff',
+		// flexDirection: 'row',
+		backgroundColor: 'red',
+		justifyContent: 'space-around',
+		alignItems: 'center',
+		gap: 10,
+		// margin: 10,
+		padding: 10,
+	},
+	smallBox: {
+		// flex: 1,
+		height: 50,
+		width: 50,
+		borderRadius: 10,
+	},
+
+	bigBox: {
+		// flex: 2,
+		height: 100,
+		width: 100,
+		borderRadius: 20,
 	},
 });
