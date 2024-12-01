b11540702106957900641c57bac3a11b4993f3c2
David Grammatico <david.lugdunum@gmail.com>
2024-12-01 16:35:31 +0100

    add text page

--- src/app/text.tsx
+++ src/app/text.tsx
@@ -1,6 +1,6 @@
 import { capitalizeFirstLetter } from '@/lib/formatString';
 import { Stack, usePathname } from 'expo-router';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, View, Text } from 'react-native';
 
 export default function TextScreen() {
 	const nameOfScreen = usePathname();
@@ -8,13 +8,51 @@ export default function TextScreen() {
 	return (
 		<View style={styles.container}>
 			<Stack.Screen options={{ title: capitalizeFirstLetter(nameOfScreen) }} />
+			<Text style={styles.text}>Ceci est un texte en ligne</Text>
+			<Text style={styles.shadow}>Shadow</Text>
+			<Text style={styles.decoration}>Decoration</Text>
+			<Text
+				onPress={() => alert('on press')}
+				style={styles.text}>
+				Ceci est un texte important
+				<Text style={styles.importantText}> en gras et rouge</Text>
+			</Text>
 		</View>
 	);
 }
+
+// 1. Les textes uniquement dans un composant <Text>
+// 2. Le text est affiché en ligne et non en flexBox (comme la <View />)
+// 3. Le style => text, decoration, shadow
+// 4. Text imbriqué avec héritage du style
+// 5. les intéractions de l'utilisateur
+// 6. accessibilité
 
 const styles = StyleSheet.create({
 	container: {
 		flex: 1,
 		backgroundColor: '#ffffff',
+		padding: 10,
+	},
+	text: {
+		fontSize: 16,
+		fontStyle: 'italic',
+		fontWeight: '600',
+		color: 'gray',
+	},
+	shadow: {
+		textShadowOffset: { width: 2, height: 2 },
+		textShadowRadius: 2,
+		textShadowColor: 'gray',
+		opacity: 0.6,
+	},
+	decoration: {
+		textDecorationLine: 'underline',
+		textDecorationColor: 'gray',
+		textDecorationStyle: 'solid',
+	},
+	importantText: {
+		color: 'red',
+		fontWeight: 'bold',
 	},
 });
