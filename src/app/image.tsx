8e96d6d03221ace47139c3812ba1e2cb0d750bdf
David Grammatico <david.lugdunum@gmail.com>
2024-12-02 15:25:28 +0100

    content for image component

--- src/app/image.tsx
+++ src/app/image.tsx
@@ -1,6 +1,9 @@
 import { capitalizeFirstLetter } from '@/lib/formatString';
 import { Stack, usePathname } from 'expo-router';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, View, Image } from 'react-native';
+
+const imageFromNetwork =
+	'https://upload.wikimedia.org/wikipedia/commons/thumb/f/fd/Chewbacca_%26_Jawa.jpg/440px-Chewbacca_%26_Jawa.jpg';
 
 export default function ImageScreen() {
 	const nameOfScreen = usePathname();
@@ -8,13 +11,36 @@ export default function ImageScreen() {
 	return (
 		<View style={styles.container}>
 			<Stack.Screen options={{ title: capitalizeFirstLetter(nameOfScreen) }} />
+			<Image
+				source={require('assets/dame-robe-rouge.jpg')}
+				style={styles.image}
+			/>
+			<Image
+				// style={styles.image}
+				source={{
+					uri: imageFromNetwork,
+					width: 300,
+					height: 200,
+				}}
+				// width={300}
+				// height={200}
+			/>
 		</View>
 	);
 }
+
+// 1. Introduction
+// 2 - Image from local
+// 3 - Image from network (3 ways to set width and heigh:t)
 
 const styles = StyleSheet.create({
 	container: {
 		flex: 1,
 		backgroundColor: '#ffffff',
+		gap: 10,
+	},
+	image: {
+		width: 300,
+		height: 200,
 	},
 });
