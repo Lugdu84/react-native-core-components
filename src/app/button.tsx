22dccb0fe61c4f674b9d3ce4433e37a679d6e308
David Grammatico <david.lugdunum@gmail.com>
2024-12-05 15:40:20 +0100

    add content for button

--- src/app/button.tsx
+++ src/app/button.tsx
@@ -1,6 +1,6 @@
 import { capitalizeFirstLetter } from '@/lib/formatString';
 import { Stack, usePathname } from 'expo-router';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, View, Button } from 'react-native';
 
 export default function ButtonScreen() {
 	const nameOfScreen = usePathname();
@@ -8,6 +8,26 @@ export default function ButtonScreen() {
 	return (
 		<View style={styles.container}>
 			<Stack.Screen options={{ title: capitalizeFirstLetter(nameOfScreen) }} />
+			<View style={styles.buttons}>
+				<View style={styles.buttonContainer}>
+					<Button
+						title="Validez"
+						color={'green'}
+						disabled={false}
+						accessibilityLabel="Boutton pour validez"
+						onPress={() => alert('Validez')}
+					/>
+				</View>
+				<View style={styles.buttonContainer}>
+					<Button
+						title="Annulez"
+						color={'red'}
+						disabled={false}
+						accessibilityLabel="Boutton pour annulez"
+						onPress={() => alert('annulez')}
+					/>
+				</View>
+			</View>
 		</View>
 	);
 }
@@ -16,5 +36,14 @@ const styles = StyleSheet.create({
 	container: {
 		flex: 1,
 		backgroundColor: '#ffffff',
+		padding: 10,
+	},
+	buttons: {
+		flexDirection: 'row',
+		gap: 10,
+		// backgroundColor: 'blue',
+	},
+	buttonContainer: {
+		flex: 1,
 	},
 });
