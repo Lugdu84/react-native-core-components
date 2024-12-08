26aa65854b62b32806ad527fadd01d74287bc5cb
David Grammatico <david.lugdunum@gmail.com>
2024-12-08 20:27:27 +0100

    add content for switch

--- src/app/switch.tsx
+++ src/app/switch.tsx
@@ -1,13 +1,24 @@
 import { capitalizeFirstLetter } from '@/lib/formatString';
 import { Stack, usePathname } from 'expo-router';
-import { StyleSheet, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Switch, View, Text } from 'react-native';
 
 export default function SwitchScreen() {
 	const nameOfScreen = usePathname();
 
+	const [isEnabled, setIsEnabled] = useState(false);
+
 	return (
 		<View style={styles.container}>
 			<Stack.Screen options={{ title: capitalizeFirstLetter(nameOfScreen) }} />
+			<Switch
+				value={isEnabled}
+				onValueChange={setIsEnabled}
+				trackColor={{ false: 'yellow', true: 'lightgreen' }}
+				thumbColor={isEnabled ? 'green' : 'red'}
+				ios_backgroundColor="yellow"
+			/>
+			<Text>Valeur du switch : {isEnabled ? 'Vrai' : 'Faux'}</Text>
 		</View>
 	);
 }
@@ -16,5 +27,6 @@ const styles = StyleSheet.create({
 	container: {
 		flex: 1,
 		backgroundColor: '#ffffff',
+		padding: 20,
 	},
 });
