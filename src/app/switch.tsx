2d3a32644012ed7e9493a04dc685de306df50fde
David Grammatico <david.lugdunum@gmail.com>
2024-12-09 14:31:47 +0100

    add content for switch component

--- src/app/switch.tsx
+++ src/app/switch.tsx
@@ -1,7 +1,7 @@
 import { capitalizeFirstLetter } from '@/lib/formatString';
 import { Stack, usePathname } from 'expo-router';
 import { useState } from 'react';
-import { StyleSheet, Switch, View, Text } from 'react-native';
+import { StyleSheet, View, Switch, Text } from 'react-native';
 
 export default function SwitchScreen() {
 	const nameOfScreen = usePathname();
@@ -14,14 +14,22 @@ export default function SwitchScreen() {
 			<Switch
 				value={isEnabled}
 				onValueChange={setIsEnabled}
-				trackColor={{ false: 'yellow', true: 'lightgreen' }}
-				thumbColor={isEnabled ? 'green' : 'red'}
-				ios_backgroundColor="yellow"
+				trackColor={{ false: 'red', true: 'blue' }}
+				ios_backgroundColor="red"
+				thumbColor={isEnabled ? 'green' : 'lightgrey'}
+				onChange={(event) => console.log(event.nativeEvent.value)}
 			/>
+
 			<Text>Valeur du switch : {isEnabled ? 'Vrai' : 'Faux'}</Text>
 		</View>
 	);
 }
+
+// 1. composant contrôlé => value et onValueChange
+// 2. utiliser le hook useState pour gérer l'état du switch
+// 3. trackColor : couleur de la barre de fond
+// 4. ios_backgroundColor : couleur de fond sur iOS
+// 5. thumbColor : couleur du bouton
 
 const styles = StyleSheet.create({
 	container: {
